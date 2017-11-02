8d74144e0a9b526acfb30b82b5b7917fc2a0ac69
HughM <hughm@foxmail.com>
2017-11-02 18:41:45 +0800

    1.提交下

--- Giraffe/src/main/java/com/hu/giraffe/activity/GiraffeActivity.java
+++ Giraffe/src/main/java/com/hu/giraffe/activity/GiraffeActivity.java
@@ -1,14 +1,29 @@
 package com.hu.giraffe.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.hu.giraffe.R;
+import com.hu.giraffe.utils.LogUtil;
 import com.hu.widget.StatusBarCompat;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class GiraffeActivity extends BaseActivity {
 
@@ -48,5 +63,111 @@ public class GiraffeActivity extends BaseActivity {
     }
 
     //--------------------------------------
+
+    private void rxDemoZero(){
+        LogUtil.v("rxDemoZero");
+        String[] numbers={"zero","one","two","three","four","five"};
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+
+            }
+        }).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        Observable.just(1,2,3,4,5)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+        //网络嵌套的请求示例
+//        getToken()
+//                .flatMap(new Func1<String, Observable<User>>() {
+//                             @Override
+//                             public Observable<User> onNext(String token) {
+//                                 return getUser(token, userId);
+//                             })
+//                                     .observeOn(AndroidSchedulers.mainThread())
+//                                     .subscribe(new Observer<User>() {
+//                                 @Override
+//                                 public void onNext(User user) {
+//                                     userView.setUser(user);
+//                                 }
+//
+//                                 @Override
+//                                 public void onCompleted() {
+//                                 }
+//
+//                                 @Override
+//                                 public void onError(Throwable error) {
+//                                     // Error handling
+//            ...
+//                                 }
+//                             });
+
+    }
+
+    //----------flatMap()变换
+    //观察者
+    Subscriber<String> subscriber=new Subscriber<String>() {
+        @Override
+        public void onSubscribe(Subscription s) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+
+        }
+
+        @Override
+        public void onError(Throwable t) {
+
+        }
+
+        @Override
+        public void onComplete() {
+
+        }
+    };
 
 }
